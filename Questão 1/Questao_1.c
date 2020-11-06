5a55ce31e86ee1ab1733ceafaee90028aeffec51
wssantiago <wssf@cin.ufpe.br>
2020-11-06 15:22:07 -0300

    comentando 1 e 2

--- Questão 1/Questao_1.c
+++ Questão 1/Questao_1.c
@@ -10,25 +10,23 @@ int MAXNUM = 1000000;
 pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
 
   
-// função de incrementar o contador 
+//Função de incrementar o contador. 
 void *inc(void *id) 
 { 
-  int tid = *((int *)id);
-  // Incrementa o valor do contador até chegar em seu valor máximo (1.000.000)
-  // Utilização de mutex para alternar as threads que incrementam o contador
-  // LOCK e UNLOCK-> NÃO DEIXA QUE OUTRAS THREADS ALTEREM A VARIÁVEL count SIMULTANEAMENTE
-  
-  while(count < MAXNUM){
-    pthread_mutex_lock(&mymutex);
-    if(++count == MAXNUM) {
-      printf("Thread %ld chegou ao número.\n", (long) tid);
-      pthread_mutex_unlock(&mymutex);
-      pthread_exit(NULL);
-    }
-    pthread_mutex_unlock(&mymutex);
-  }
-  return NULL;
-} 
+
+	int tid = *((int *)id);
+	// Incrementa o valor do contador até chegar em seu valor máximo (1.000.000).
+	// Utilização de mutex e funções lock/unlock para garantir a exclusão mútua relacionada ao contador.
+	while(count < MAXNUM){
+		pthread_mutex_lock(&mymutex);
+		if(++count == MAXNUM) {
+			printf("Thread %ld chegou ao numero.\n", (long) tid);
+			pthread_mutex_unlock(&mymutex);
+			pthread_exit(NULL);
+		}
+		pthread_mutex_unlock(&mymutex);
+	}
+}
   
 int main() 
 { 
@@ -37,27 +35,32 @@ int main()
     printf("Digite o valor de N para quantas threads incrementaram o contador: ");
     scanf("%d",&n);
 
-    pthread_t *threads = (pthread_t *) malloc(n*sizeof(pthread_t)); //cria array de threads
-    int **threadsId = (int **) malloc(n*sizeof(int *));//cria array de ponteiros para id das threads
+	//Cria-se array de threads.
+    pthread_t *threads = (pthread_t *) malloc(n*sizeof(pthread_t));
+
+	//Cria-se array de inteiros que será utilizado para identificar cada thread criada.
+    int *threadsId = (int *) malloc(n*sizeof(int));
   
-    // Cria N threads 
+    //Criam-se n threads de acordo com a função inc, cujo parâmetro
+	//corresponde ao respectivo threadsId[i]. 
     for (i = 0; i < n; i++) {
-      threadsId[i] = (int *) malloc(sizeof(int));
-      *(threadsId[i]) = i;
-      int rc = pthread_create(&threads[i], NULL, inc, (void *)threadsId[i]);
-
-      if(rc) {
-        printf("ERRO na criação da thread[%d], código de retorno: %d\n", *(threadsId[i]), rc);
-        exit(-1);
-      }
+    	threadsId[i] = i;
+    	int rc = pthread_create(&threads[i], NULL, inc, (void *) &threadsId[i]);
+    	if(rc) {
+    		printf("ERRO na criação da thread[%d], código de retorno: %d\n", threadsId[i], rc);
+    		exit(-1);
+    	}
     }
 
-    pthread_exit(NULL);
+	//Uso do join para esperar a finalização da execução de todas as threads criadas.
     for(i=0; i<n; i++) {
-      free(threadsId[i]);
+    	pthread_join(threads[i], NULL);
     }
+
+	//Liberação de memória alocada dinamicamente.
     free(threadsId);
     free(threads);
 
+    pthread_exit(NULL);
     return 0; 
 }
--- Questão 2/Questao_2.c
+++ Questão 2/Questao_2.c
@@ -8,20 +8,27 @@ char colores[][7] = {"\e[40m", "\e[41m",
 "\e[42m", "\e[43m", "\e[44m", "\e[45m", "\e[46m",
 "\e[47m"};
 
+//Struct que representa a tela a ser atualizada pelas threads.
 typedef struct {
     int linha;
     char *text;
 } msg;
+
+//Struct que guarda uma lista de nomes de arquivos e seu tamanho.
+typedef struct {
+    int size;
+    char **files;
+} cmd;
 
 pthread_mutex_t printMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t *mutexes;
 pthread_t *threads;
 int L;
 msg *tela;
-char** filenames;
+cmd *filenamesPerThread;
 
-void delay(int number_of_seconds) 
-{ 
+//Função que executa delay.
+void delay(int number_of_seconds) { 
     int milli_seconds = 1000 * number_of_seconds; 
     clock_t start_time = clock(); 
   
@@ -35,65 +42,110 @@ void printLinhaTela(int index) {
     printf("\e[0m");
 }
 
-void *func(void *cmd) {
+//Função passada na criação das threads.
+void *func(void *command) {
     delay(2000);
-    char *command = *((char **)cmd);
+    cmd filesArr = *((cmd *)command);
+    int k;
 
-    FILE *arq = fopen(command, "r");
-    if(arq == NULL) {
-        printf("ERRO ao tentar abrir arquivo %s\n", command);
-        exit(-1);
-    }
-
-    int size = 0;
-    int linha;
-    char text[100];
-    msg *dados = NULL;
-
-    while(fscanf(arq, "%d %[^\n]", &linha, text) != EOF) {
-        dados = (msg *) realloc(dados, (1+size)*sizeof(msg));
-        dados[size].linha = linha;
-        dados[size].text = (char *) malloc(strlen(text));
-        strcpy(dados[size].text, text);
-        size++;
-    }
-    fclose(arq);
-
-    int i;
-    for(i=0; i<size; i++) {
-        pthread_mutex_lock(&mutexes[(dados[i].linha)-1]);
-        tela[(dados[i].linha)-1].text = (char *) malloc(strlen(dados[i].text));
-        strcpy(tela[(dados[i].linha)-1].text, dados[i].text);
-
-        //pthread_mutex_lock(&printMutex);
-        int j;
-        for(j=0; j<L; j++) {
-          printLinhaTela(j);
+    //Para cada string na lista de strings de filesArr, lê-se o conteúdo do arquivo, cujo nome corresponde à string corrente
+    //e faz-se as devidas atualizações na tela respeitando a exclusão mútua por linha.
+    for(k=0; k<filesArr.size; k++) {
+        FILE *arq = fopen(filesArr.files[k], "r");
+        if(arq == NULL) {
+            printf("ERRO ao tentar abrir arquivo.\n");
+            exit(-1);
         }
-        printf("\n");
-        //pthread_mutex_unlock(&printMutex);
 
-        delay(4500);
-        system("clear");
-        pthread_mutex_unlock(&mutexes[(dados[i].linha)-1]);
+        int size = 0;
+        int linha;
+        char text[100];
+        msg *dados = NULL;
 
+        //Leitura do arquivo respeitando o intervalo [1, L], onde L é a quantidade de linhas na tela.
+        //Caso o intervalo não seja respeitado, a linha correspondente é desconsiderada na leitura do
+        //arquivo em questão.
+        while(fscanf(arq, "%d %[^\n]", &linha, text) != EOF) {
+            dados = (msg *) realloc(dados, (1+size)*sizeof(msg));
+            if(linha < 1 || linha > L) {
+                printf("Valor para linha invalido. Esta sera desconsiderada.\n");
+            }
+            else {
+                dados[size].linha = linha;
+                dados[size].text = (char *) malloc(strlen(text));
+                strcpy(dados[size].text, text);
+                size++;
+            }
+        }
+        fclose(arq);
+
+        //Atualização da tela de acordo com a quantidade de alterações (size) calculadas para cada arquivo.
+        //Essa atualização é feita de acordo com os dados lidos de cada arquivo e respeita a exclusão mútua
+        //por linha. A linha correspondente é printada após a alteração e é adicionado um delay de 3 segundos
+        //até que a próxima alteração seja feita.
+        int i;
+        for(i=0; i<size; i++) {
+            pthread_mutex_lock(&mutexes[(dados[i].linha)-1]);
+            tela[(dados[i].linha)-1].text = (char *) malloc(strlen(dados[i].text));
+            strcpy(tela[(dados[i].linha)-1].text, dados[i].text);
+
+            pthread_mutex_lock(&printMutex);
+            int j;
+            for(j=0; j<L; j++) {
+              printLinhaTela(j);
+            }
+            printf("\n");
+            pthread_mutex_unlock(&printMutex);
+
+            delay(3000);
+            system("clear");
+            pthread_mutex_unlock(&mutexes[(dados[i].linha)-1]);
+
+        }
+
+        free(dados);
     }
-
-    free(dados);
+    
 }
 
-void init(int L, int qtdThreads) {
+//Função responsável por inicializar as estruturas que serão utilizadas ao longo do código.
+void init(int L, int qtdArq, int qtdThreads) {
     threads = (pthread_t *) malloc(qtdThreads*sizeof(pthread_t));
     mutexes = (pthread_mutex_t *) malloc(L*sizeof(pthread_mutex_t));
     tela = (msg *) malloc(L*sizeof(msg));
-    filenames = (char **) malloc(qtdThreads*sizeof(char *));
-
-    int i;
-
+    filenamesPerThread = (cmd *) malloc(qtdThreads*sizeof(cmd));
+    
+    int sizeFilePerThread = qtdArq/qtdThreads;
+    int i,j;
+    int k=0;
+    
+    //Aqui é feito uma divisão, entre as threads, de todos os arquivos que serão analisados.
+    //De acordo com essa política, a última thread sempre ficará com um maior número de arquivos
+    //para analisar. Isso ocorre se qtdArq % qtdThreads != 0.
     for(i=0; i<qtdThreads; i++) {
-        filenames[i] = (char *) malloc(50);
+        if(i+1 == qtdThreads) {
+            int tam = sizeFilePerThread + (qtdArq%qtdThreads);
+            filenamesPerThread[i].files = (char **) malloc(tam*(sizeof(char *)));
+            for(j=0; j<tam; j++) {
+                printf("Digite o nome do arquivo[%d] a ser lido: ", k++);
+                filenamesPerThread[i].files[j] = (char *) malloc(50);
+                scanf(" %[^\n]", filenamesPerThread[i].files[j]);
+            }
+            filenamesPerThread[i].size = tam;
+        }
+        else {
+            filenamesPerThread[i].files = (char **) malloc(sizeFilePerThread*(sizeof(char *)));
+            for(j=0; j<sizeFilePerThread; j++) {
+                printf("Digite o nome do arquivo[%d] a ser lido: ", k++);
+                filenamesPerThread[i].files[j] = (char *) malloc(50);
+                scanf(" %[^\n]", filenamesPerThread[i].files[j]);
+            }
+            filenamesPerThread[i].size = sizeFilePerThread;
+        }
     }
 
+    //Inicialização da tela responsável por mostrar as alterações.
+    //Aqui também é inicializado o array de mutexes.
     for(i=0; i<L; i++) {
         tela[i].linha = i;
         tela[i].text = (char *) malloc(6);
@@ -105,32 +157,46 @@ void init(int L, int qtdThreads) {
 
 
 int main() {
+    int i, j;
+
+    //Lê-se o números de arquivos a serem tratados.
     int nArq;
     printf("Digite o numero de arquivos: ");
     scanf("%d", &nArq);
 
-    int i, j; 
+    //Lê-se a quantidade de threads, e, caso não respeite o intervalo,
+    //o usuário é novamente direcionado a entrar um valor válido.
+    int nThreads;
+    printf("Digite a quantidade de threads (1 < T <= %d) a serem executadas: ", nArq);
+    scanf("%d", &nThreads);
+    while(nThreads > nArq || nThreads <= 1) {
+        printf("Quantidade de threads invalida. Entre com o numero de threads (1 < T <= %d): ", nArq);
+        scanf("%d", &nThreads);
+    } 
 
     printf("Digite a quantidade de linhas da tela: ");
     scanf("%d", &L);     
-    init(L, nArq);
+    
+    //init é chamado de acordo com as variáveis já lidas.
+    init(L, nArq, nThreads);
 
-    for(i=0; i<nArq; i++) {
-        printf("Digite o nome do arquivo[%d] a ser lido: ", i);
-        scanf(" %[^\n]", filenames[i]);
-    }
-
-    int tam;
-    for(i=0; i<nArq; i++) {
-
-        int rc = pthread_create(&threads[i], NULL, func, (void *) &filenames[i]);
+    //nThreads são criadas de acordo com a função func, que recebe como
+    //parâmetro uma struct do tipo "cmd". Dessa maneira, a thread criada
+    //recebe uma lista de strings, relacionadas aos nomes dos arquivos
+    //correspondentes, e seu respectivo tamanho, para que a iteração
+    //seja possível.
+    for(i=0; i<nThreads; i++) {
+        int rc = pthread_create(&threads[i], NULL, func, (void *) &filenamesPerThread[i]);
         if(rc) printf("ERRO na criacao da thread[%d], codigo de retorno: %d\n", i, rc);
     }
 
-    for(i=0; i<nArq; i++) {
+    //main espera a finalização de cada thread criada, para que se tenha
+    //certeza de que todas as alterações válidas foram feitas na tela.
+    for(i=0; i<nThreads; i++) {
         pthread_join(threads[i], NULL);
     }
     
+    //Printa pela última vez a tela já finalizada com todas as alterações.
     for(i=0; i < L; i++) {
         printf("%d", tela[i].linha);
         printf("%s", colores[i]);        
@@ -138,15 +204,10 @@ int main() {
         printf("\e[0m");
     }
 
-
-
+    //Liberação da memória alocada dinamicamente ao longo do código.
     free(threads);
     free(mutexes);
-    for(i=0; i<nArq; i++) {
-        free(filenames[i]);
-    }
-    free(filenames);
-
+    free(filenamesPerThread);
     for(i=0; i<L; i++) {
         free(tela[i].text);
     }
