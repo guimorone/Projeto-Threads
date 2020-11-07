65119a35616cfa7b0627b52f5aad27511880f717
Williams Santiago <50757842+wssantiago@users.noreply.github.com>
2020-11-07 12:47:27 -0300

    comentando 7

--- Questão 7/Questao_7.c
+++ Questão 7/Questao_7.c
@@ -3,14 +3,22 @@
 #include <pthread.h>
 #include <string.h> 
 
+//Struct responsável por guardar os valores RGB bem como seu correspondente C.
+//Além disso, a estrutura guarda valores [i][j] relacionados à posição do pixel
+//na matriz.
 typedef struct {
     int R, G, B;
     int C;
     int i, j;
 } rgb;
 
+//Variável responsável por guardar as informações do arquivo *.ppm de entrada
+//em estruturas do tipo 'rgb' para cada pixel da matriz representada por esse arquivo.
 rgb **mat;
 
+//Função passada como argumento na criação das threads. Ela recebe como argumento
+//uma estrutura do tipo 'rgb'. Essa função apenas atualiza o valor de C corres-
+//pondente à estrutura parâmetro e o salva.
 void *func(void *cmd) {
     rgb command = *((rgb *) cmd);
 
@@ -18,19 +26,16 @@ void *func(void *cmd) {
     mat[command.i][command.j].C = C;
 }
 
+
+//Função inicializadora. Alocação e inicializaçãoda variável 'mat' de acordo
+//com as dimensões da matriz determinada em "void leArquivo(int *l, int *c, int *max)".
 void init(int l, int c) {
     mat = (rgb **) malloc(l*sizeof(rgb *));
-    if(mat == NULL) {
-        printf("erro na alocacao\n");
-        exit(-1);
-    }
+
     int i, j;
     for(i=0; i<l; i++) {
         mat[i] = (rgb *) malloc(c*sizeof(rgb));
-        if(mat[i] == NULL) {
-            printf("erro na alocacao %d\n", i);
-            exit(-1);
-        }
+
         for(j=0; j<c; j++) {
             mat[i][j].R = 0;
             mat[i][j].G = 0;
@@ -42,6 +47,8 @@ void init(int l, int c) {
     }
 }
 
+//Função responsável pela leitura de um arquivo entrada do tipo *.ppm. Os dados lidos
+//são salvos em 'mat'.
 void leArquivo(int *l, int *c, int *max) {
     int i, j;
 
@@ -57,12 +64,18 @@ void leArquivo(int *l, int *c, int *max) {
 
     char formato[5];
     fscanf(arq, " %[^\n]", formato);
-
+    
+    //Prossegue se o formato for o adequado.
     if(!(strcmp(formato, "P3"))) {
         fscanf(arq, "%d %d", c, l);
         fscanf(arq, "%d", max);
+        
+        //Inicializa 'mat'.
         init(*l, *c);
-
+        
+        //Faz a leitura dos valores RGB do arquivo ao passo que salva esses dados em 'mat'.
+        //Os valores de [i][j] são também salvos uma vez que serão necessários para organizar
+        //a escrita do arquivo *.ppm de sáida.
         for(i=0; i< (*l); i++) {
             for(j=0; j< (*c); j++) {
                 int r, g, b;
@@ -77,17 +90,15 @@ void leArquivo(int *l, int *c, int *max) {
             }
         }
     }
+    
     else printf("Erro na formatacao do arquivo ppm.\n");
     fclose(arq);
-
-    for(i=0; i<(*l); i++) {
-        for(j=0; j<(*c); j++) {
-            printf("%d %d %d %d //", mat[i][j].R, mat[i][j].G, mat[i][j].B, mat[i][j].C);
-        }
-        printf("\n");
-    }
 }
 
+//Função responsável por escrever os valores de C, correspondentes aos respectivos
+//RGB de cada pixel da matriz de entrada, em um arquivo de saída do tipo *.ppm.
+//É válido ressaltar que o arquivo só é escrito após todas as threads finalizarem
+//sua execução, garantido que todos os pixels tiveram seus valores de C calculados.
 void escreveArquivo(int l, int c, int max) {
     int i, j;
     
@@ -100,9 +111,11 @@ void escreveArquivo(int l, int c, int max) {
         printf("ERRO ao criar arquivo");
             exit(-1);
     }
-
+    
+    //Escrita do formato, quantidade de linhas e colunas e valor máximo.
     fprintf(arq, "%s\n%d %d\n%d\n", "P3", c, l, max);
-
+    
+    //Escrita dos valores de C.
     for(i=0; i<l; i++) {
         for(j=0; j<c; j++) {
             fprintf(arq, "%d %d %d\n", mat[i][j].C, mat[i][j].C, mat[i][j].C);
@@ -116,13 +129,23 @@ void escreveArquivo(int l, int c, int max) {
 int main() {
     int max;
     int linha, coluna;
+    
+    //Leitura do arquivo de entrada, salvando as dimensões da matriz bem como o valor
+    //RGB máximo para cada pixel.
     leArquivo(&linha, &coluna, &max);
 
     int k=0, i, j;
-
+    
+    //Cria-se array de estruturas do tipo 'rgb'. Esse array será utilizado para passar
+    //as estruturas como parâmetro de 'func' na criação de cada thread.
     rgb *msg = (rgb *) malloc((linha*coluna)*sizeof(rgb));
+    
+    //Cria-se array de (linha*coluna) threads, ou seja, uma thread para tratar cada pixel da matriz.
     pthread_t *threads = (pthread_t *) malloc((linha*coluna)*sizeof(pthread_t));
-
+    
+    //Atualização do array 'msg' de acordo com os valores lidos e salvos em 'mat'.
+    //A ideia é que as threads apenas modificarão os valores C, correspondentes a cada pixel,
+    //da matriz 'mat'.
     for(i=0; i<linha; i++) {
         for(j=0; j<coluna; j++) {
             msg[k].R = mat[i][j].R;
@@ -131,27 +154,32 @@ int main() {
             msg[k].C = mat[i][j].C;
             msg[k].i = mat[i][j].i;
             msg[k].j = mat[i][j].j;
-
+            
+            //Criação das threads.
             int rc = pthread_create(&threads[k], NULL, func, (void *) &msg[k]);
             if(rc) printf("Erro na criacao da thread[%d], codigo de retorno: %d\n", i, rc);
             k++;
 
         }
     }
-
+    
+    //Esperam-se todas as threads finalizarem sua execução para que todos os pixels tenham
+    //seus respectivos valores C atualizados.
     for(i=0; i<(linha*coluna); i++) {
         pthread_join(threads[i], NULL);
-        printf("Esperando thread[%d]\n", i);
     }
-
+    
+    //O resultado salvo em 'mat' é escrito em um arquivo de saída do tipo *.ppm.
     escreveArquivo(linha, coluna, max);
-
+    
+    //Liberação da memória alocada dinamicamente ao longo do código.
     free(msg);
     free(threads);
     for(i=0; i<linha; i++) {
         free(mat[i]);
     }
     free(mat);
-
+    
+    pthread_exit(NULL);
     return 0;
 }
